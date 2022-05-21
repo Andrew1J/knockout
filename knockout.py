f9452f3d3a9a5f586e61d97cd8bef1dd91ee58cc
Andrew Juang <andrewjuang@Andrews-MacBook-Air.local>
2022-05-20 21:14:40 -0400

    will fix later when im not tired

--- knockout.py
+++ knockout.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 pygame.init()
 
 # Global Variables
@@ -10,12 +11,13 @@ screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pucks = pygame.sprite.Group()
 
 # Defines a class for the pucks
-class Puck(object):
-    def __init__(self):
-        self.position = (250,250)
-        self.color = (255,0,255)
+class Puck(pygame.sprite.Sprite):
+    def __init__(self, position, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.position = position
+        self.color = color
         self.onIsland = True
-
+        self.isClicked = False
 
     def set_pos(self, x, y):
         ''' Set the x,y position of the puck '''
@@ -29,17 +31,22 @@ class Puck(object):
         ''' Calculate the velocities and stuff '''
         pass
 
+    def click(self):
+        self.isClicked = not self.isClicked
+        self.set_color((255,255,255))
+
     def draw(self, surface):
         ''' Draw puck to the surface '''
         pygame.draw.circle(surface, self.color, (self.position[0], self.position[1]), PUCK_RADIUS)
 
     def get_pos(self):
         '''Get puck position'''
-        return (self.positions[0],self.position[1])
+        return (self.position[0],self.position[1])
 
     def col(self, pucks):
         '''Checking for collision'''
         return (self, pygame.sprite.spritecollideany(self, pucks, collided = None))
+
 
 # Defines a class for the shooting arrow
 class Arrow(object):
@@ -55,6 +62,7 @@ class Arrow(object):
 # Set Up Levels
 def setup_lvl1():
     ''' Sets up screen for level 1'''
+
     # Draw Water
     screen.fill((0, 0, 255))
 
@@ -62,23 +70,19 @@ def setup_lvl1():
     pygame.draw.rect(screen, (0,255,0), [SCREEN_WIDTH/2 - ISLAND_WIDTH/2, SCREEN_HEIGHT/2 - ISLAND_HEIGHT/2, ISLAND_WIDTH, ISLAND_HEIGHT])
 
     # Draw Pucks
-    puck1 = Puck()
-    pucks.add(puck1)
-    puck2 = Puck()
-    puck3 = Puck()
-    puck4 = Puck()
-    puck5 = Puck()
-    puck6 = Puck()
+    puck1 = Puck((300, 500), (255,0,255))
+    puck2 = Puck((300, 400), (255,0,255))
+    puck3 = Puck((300, 300), (255,0,255))
+    puck4 = Puck((500, 500), (255,0,0))
+    puck5 = Puck((500, 400), (255,0,0))
+    puck6 = Puck((500, 300), (255,0,0))
 
-    puck1.set_pos(300, 500)
-    puck2.set_pos(300, 400)
-    puck3.set_pos(300, 300)
-    puck4.set_pos(500, 500)
-    puck5.set_pos(500, 400)
-    puck6.set_pos(500, 300)
-    puck4.set_color((255,0,0))
-    puck5.set_color((255,0,0))
-    puck6.set_color((255,0,0))
+    pucks.add(puck1)
+    pucks.add(puck2)
+    pucks.add(puck3)
+    pucks.add(puck4)
+    pucks.add(puck5)
+    pucks.add(puck6)
 
     puck1.draw(screen)
     puck2.draw(screen)
@@ -97,23 +101,41 @@ def main():
 
     # Set up the level
     setup_lvl1()
+    
+    SETUPSTATE = True
 
     # Run until the user asks to quit
     running = True
     while running:
-
-        # Did the user click the window close button?
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # Check for game quit()
+                pygame.quit()
                 running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN and pucks.rect.collidepoint(pygame.mouse.get_pos()):
+                pos = pygame.mouse.get_pos()
+
+
+            
 
         # Collision detection
         for puck in pucks:
-            col(puck, pucks)
+            # col(puck, pucks)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+
+        if SETUPSTATE:
+            pass
+            # able to click on balls and draw arrows
+        else:
+            pass
+            # balls shoot
 
         # Flip the display
         pygame.display.flip()
         pygame.display.update()
 
+    pygame.quit()
 if __name__ == '__main__':
     main()
