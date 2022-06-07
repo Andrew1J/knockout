8e0bb9dfa2f98a664bb6d36530ad4a21e27fb823
Andrew <andrewjuang01@gmail.com>
2022-06-07 08:11:28 -0400

    game restart bug fix

--- knockout.py
+++ knockout.py
@@ -448,7 +448,7 @@ def main():
     # Game Restart
     RESTART = True
     while RESTART:
-        for event in event_list:
+        for event in pygame.event.get():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
                 PUCKS = []
                 ARROWS = []
